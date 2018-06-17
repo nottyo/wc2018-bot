a8d99679cc46bdbd2a8ffe0ac6fb6f0fc3d02a97
traitanit-h <traitanit.h@linecorp.com>
2018-06-17 15:41:03 +0700

    add bot feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, abort
-from datetime import datetime
+import requests
+from datetime import datetime, date, timedelta
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -20,6 +21,9 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+wc_api = 'http://api.football-data.org/v1/competitions/467'
+wc_api_key = os.getenv('FOOTBALL_API_KEY', None)
+wc_api_headers = {'X-Auth-Token': wc_api_key}
 
 @app.route('/')
 def homepage():
@@ -49,11 +53,144 @@ def callback():
 
     return 'OK'
 
+def getFixtures():
+    url = wc_api + '/fixtures'
+    response = requests.get(url, headers=wc_api_headers)
+    return response
+
+def handleWorldCupResult():
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        for fixture in json['fixtures']:
+            if fixture['status'] == 'FINISHED':
+                result = fixture['result']
+                text += '[FT] ' + fixture['homeTeamName'] + ' ' + str(result['goalsHomeTeam']) + ' - ' + str(result['goalsAwayTeam']) + ' ' + fixture['awayTeamName']
+                text += '\n' 
+        return text    
+    return None
+
+def handleYesterdayWCResult():
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        for fixture in json['fixtures']:
+            yesterday = date.today() - timedelta(1)
+            dt=datetime.strptime(fixture['date'],'%Y-%m-%dT%H:%M:%SZ').date()
+            if fixture['status'] == 'FINISHED' and yesterday == dt:
+                result = fixture['result']
+                text += '[FT] ' + fixture['homeTeamName'] + ' ' + str(result['goalsHomeTeam']) + ' - ' + str(result['goalsAwayTeam']) + ' ' + fixture['awayTeamName']
+                text += '\n' 
+        return text    
+    return None
+
+def handleLiveScore():
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        for fixture in json['fixtures']:
+            if fixture['status'] == 'IN_PLAY':
+                result = fixture['result']
+                text += '[LIVE] ' + fixture['homeTeamName'] + ' ' + str(result['goalsHomeTeam']) + ' - ' + str(result['goalsAwayTeam']) + ' ' + fixture['awayTeamName']
+                text += '\n' 
+        if text != "":
+            return text
+        else:
+            return "ไม่มีบอลเตะตอนนี้นะครับ"
+    return None
+
+def handleFixtures(): 
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        data = {}
+        for fixture in json['fixtures']:
+            if fixture['status'] == 'TIMED':
+                dt=datetime.strptime(fixture['date'],'%Y-%m-%dT%H:%M:%SZ')
+                play_time = dt + timedelta(hours=7)
+                matches_text = fixture['homeTeamName'] + ' vs. ' + fixture['awayTeamName'] + " " + play_time.strftime("%H:%M")
+                if dt.date() not in data:
+                    data[dt.date()] = [matches_text]
+                else:
+                    data[dt.date()].append(matches_text)
+        for key, values in data.items():
+            text += key.strftime("%A %d %B %Y") + "\n"
+            for value in values:
+                text += value + "\n"
+            text += "=========================\n\n"
+        return text        
+    return None    
+
+def handleTodayFixtures(): 
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        data = {}
+        for fixture in json['fixtures']:
+            if fixture['status'] == 'TIMED':
+                dt=datetime.strptime(fixture['date'],'%Y-%m-%dT%H:%M:%SZ')
+                if dt.date() == date.today():
+                    play_time = dt + timedelta(hours=7)
+                    matches_text = fixture['homeTeamName'] + ' vs. ' + fixture['awayTeamName'] + " " + play_time.strftime("%H:%M")
+                    if dt.date() not in data:
+                        data[dt.date()] = [matches_text]
+                    else:
+                        data[dt.date()].append(matches_text)
+                
+        for key, values in data.items():
+            text += key.strftime("%A %d %B %Y") + "\n"
+            for value in values:
+                text += value + "\n"
+        if text != "":
+            return text
+        else:
+            return "ไม่มีโปรแกรมเตะสำหรับวันนี้ครับ พิมพ์ \"โปรแกรม\" เพื่อดูโปรแกรมการแข่งขันในวันอื่นๆ"      
+    return None    
+
+def handleTodayResult():
+    response = getFixtures()
+    if response.status_code == 200:
+        json = response.json()
+        text = ""
+        for fixture in json['fixtures']:
+            dt=datetime.strptime(fixture['date'],'%Y-%m-%dT%H:%M:%SZ')
+            if dt.date() == date.today():
+                if fixture['status'] == 'FINISHED':
+                    result = fixture['result']
+                    text += '[FT] ' + fixture['homeTeamName'] + ' ' + str(result['goalsHomeTeam']) + ' - ' + str(result['goalsAwayTeam']) + ' ' + fixture['awayTeamName']
+                    text += '\n' 
+        if text != "":
+            return text
+        else:
+            return "ยังไม่มีผลบอลสำหรับวันนี้ครับ"  
+    return None   
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    text = event.message.text
+    result = None
+    if 'ผลบอล' in text:
+        result = handleWorldCupResult()
+    if 'ผลบอลเมื่อคืน' in text:
+        result = handleYesterdayWCResult()
+    if 'ผลบอลตอนนี้' in text:
+        result = handleLiveScore()
+    if 'ผลบอลวันนี้' in text:
+        result = handleTodayResult()
+    if 'โปรแกรม' in text:
+        result = handleFixtures() 
+    if 'โปรแกรมวันนี้' in text:
+        result = handleTodayFixtures()
+    if result is not None:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result))
+    
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
